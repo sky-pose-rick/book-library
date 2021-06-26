a02254b6c2111503858868c6248e9458f6c8da2a
Richard Cranwell <rickycranwell@gmail.com>
2021-06-26 17:06:30 -0500

    create AddToLibrary

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,29 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(entry) {
+  myLibrary.push(entry);
+
+  let newElem = document.createElement("div");
+  newElem.classList.add("book");
+  addParagraph(newElem, "Title", entry.title);
+  addParagraph(newElem, "Author", entry.author);
+  addParagraph(newElem, "Pages", entry.pages);
+  addParagraph(newElem, "Status", entry.isRead ? "Have Read": "Not Read Yet");
+  libraryElem.appendChild(newElem);
 }
+
+function addParagraph(parent, label, content)
+{
+  let newElem = document.createElement("p");
+  newElem.innerText = `${label}: ${content}`;
+  parent.appendChild(newElem);
+}
+
+const newButton = document.getElementById("add-button");
+const newForm = document.getElementById("add-form");
+const libraryElem = document.getElementById("library");
+
+let qf = new Book("The quick brown fox", "Some guy", "5", false);
+addBookToLibrary(qf);
+addBookToLibrary(qf);
