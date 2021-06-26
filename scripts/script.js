a62efa9f9f770435bf6f53828950d1191d841536
Richard Cranwell <rickycranwell@gmail.com>
2021-06-26 17:57:53 -0500

    implement book adding form

--- scripts/script.js
+++ scripts/script.js
@@ -33,20 +33,40 @@ const newButton = document.getElementById("add-button");
 const newForm = document.getElementById("add-form");
 const libraryElem = document.getElementById("library");
 const hideButton = document.getElementById("hide-button");
-const formButton = document.getElementById("formButton");
+const formButton = document.getElementById("form-button");
 
 newButton.addEventListener("click", ()=>{
   newForm.toggleAttribute("hidden");
   newButton.toggleAttribute("hidden");
   hideButton.toggleAttribute("hidden");
-})
+  formButton.toggleAttribute("hidden");
+});
 
 hideButton.addEventListener("click", ()=>{
   newForm.toggleAttribute("hidden");
   newButton.toggleAttribute("hidden");
   hideButton.toggleAttribute("hidden");
-})
+  formButton.toggleAttribute("hidden");
+});
 
-let qf = new Book("The quick brown fox", "Some guy", "5", false);
-addBookToLibrary(qf);
-addBookToLibrary(qf);
+formButton.addEventListener("click", ()=>{
+  let children = newForm.children;
+  const title = children[1].value;
+  const author = children[4].value;
+  const pages = children[7].value;
+  const isRead = children[10].checked;
+
+  if(title === null || author === null || Number.isNaN(+pages))
+  {
+    return;
+  }
+
+  const book = new Book(title, author, pages, isRead);
+  addBookToLibrary(book);
+});
+
+if(myLibrary.length === 0)
+{
+  let qf = new Book("The quick brown fox", "Some guy", "5", false);
+  addBookToLibrary(qf);
+}
